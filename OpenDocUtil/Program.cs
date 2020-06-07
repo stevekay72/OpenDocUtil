38c61cf942bcf45a05f30daf4048cc227f7b871d
stevekay72 <stevekay72@gmail.com>
2020-06-07 19:50:39 +0100

    Working replacements
    Should be easily extendable from here

--- OpenDocUtil/Program.cs
+++ OpenDocUtil/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace OpenDocUtil
 {
@@ -6,7 +9,22 @@ namespace OpenDocUtil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string testFile = "/Users/stevekay72/Projects/OpenDocUtil/OpenDocUtil/TestDoc001.docx";
+            using (var doc = WordprocessingDocument.Open(testFile, true))
+            {
+                var mainDocPart = doc.MainDocumentPart;
+                var links = mainDocPart.HyperlinkRelationships.Where(x => x.Id != null).ToList();
+                foreach(var lnk in links)
+                {
+                    var relationId = lnk.Id;
+                    var uri = lnk.Uri.ToString();
+                    mainDocPart.DeleteReferenceRelationship(lnk);
+                    Uri newUri = new Uri(uri.Replace("google", "microsoft"));
+                    mainDocPart.AddHyperlinkRelationship(newUri, true, relationId);
+                    Console.WriteLine($"{uri} => {newUri.ToString()}");
+                }
+                doc.SaveAs(testFile.Replace("001","002"));
+            }
         }
     }
 }
