f4c3d7b17224bb25ff5f26c6fc48ed2963069397
stevekay72 <stevekay72@gmail.com>
2020-06-07 19:08:13 +0100

    Initial

---
+++ OpenDocUtil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenDocUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
